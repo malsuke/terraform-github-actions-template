793e51dee4e42d3c857f42f76e49a3cb9f0c25b4
malsuke <153903070+malsuke@users.noreply.github.com>
2024-10-05 17:18:30 +0900

    Update backend.tf

--- terraform/aws/xxxx/sample_s3/backend.tf
+++ terraform/aws/xxxx/sample_s3/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1"
 
   backend "s3" {
-    bucket         = "terraform-state-bucket-730335203034"
+    bucket         = "terraformstatebucket730335203034"
     key            = "sample_s3.tfstate"
     region         = "ap-northeast-1"
     dynamodb_table = "terraform-state-lock"
